de0f0375914cf36d13579f6cd00268a3b46bb3a2
OgawaYuna03 <yuna.ogawa.howsplan@gmail.com>
2026-04-28 13:49:18 +0900

    演習11まで

---
+++ Ex_02_08/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Ex_02_08;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        string[] station = { "品川", "大崎", "五反田", "目黒", "恵比寿", "渋谷", "原宿", "代々木", "新宿" };
+        var count = 0;
+        while (count < station.Length)
+        {
+            System.Console.WriteLine(station[count]);
+            count++;
+        }
+    }
+}
--- Example/Program.cs
+++ Example/Program.cs
@@ -4,19 +4,14 @@ static class Program
 {
     static void Main(string[] args)
     {
-        bool a = true, b = false;
-        Console.WriteLine($"a && a = {a && a}");
-        Console.WriteLine($"a && b = {a && b}");
-        Console.WriteLine($"b && a = {b && a}");
-        Console.WriteLine($"b && b = {b && b}");
-        Console.WriteLine($"a || a = {a || a}");
-        Console.WriteLine($"a || b = {a || b}");
-        Console.WriteLine($"b || a = {b || a}");
-        Console.WriteLine($"b || b = {b || b}");
-        int x = 1;
-        Console.WriteLine($"b && x++ > 1 = {b && x++ > 1}");
-        Console.WriteLine($"x = {x}");
-        Console.WriteLine($"b & x++ > 1 = {b & x++ > 1}");
-        Console.WriteLine($"x = {x}");
+        for (int i = 0; i < 6; i++)
+        {
+            if (i == 2)
+            {
+                continue;
+            }
+            Console.WriteLine($"i = {i}");
+        }
+        Console.WriteLine("終了");
     }
 }
