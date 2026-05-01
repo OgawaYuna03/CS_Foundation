d751dcb781388f8c7ad4bc69f4b6325f3c276fdd
OgawaYuna03 <yuna.ogawa.howsplan@gmail.com>
2026-05-01 15:59:14 +0900

    Ex_03_15

---
+++ Ex_03_15/Calculator.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Formats.Asn1;
+
+namespace Ex_03_15;
+
+public class Calculator
+{
+    public int Div(int x, int y)
+    {
+     return x/y;
+    }
+
+}
